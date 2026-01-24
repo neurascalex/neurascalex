72ad6667f3f1389b145d08e6e3c6aaff29352a9c
durgaprasad773 <pdurgaprasa0707@gmail.com>
2026-01-24 09:43:01 +0530

    fix the Issues

--- server.js
+++ server.js
@@ -4,24 +4,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Disable caching for all routes
-app.use((req, res, next) => {
-  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
-  res.set('Pragma', 'no-cache');
-  res.set('Expires', '0');
-  next();
-});
-
 // Serve static files from the current directory
-app.use(express.static(__dirname, {
-  etag: false,
-  lastModified: false,
-  maxAge: 0
-}));
+app.use(express.static(__dirname));
 
 // Handle SPA routing - send all requests to index.html
 app.get('*', (req, res) => {
-  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
